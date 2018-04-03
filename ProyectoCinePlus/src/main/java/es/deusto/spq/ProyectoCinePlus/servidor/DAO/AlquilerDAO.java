74f31c23a4878d11eb298e7e483538a77701737e
Xabier <xsarri9@opendeusto.es>
2018-04-03 17:23:14 +0200

    AlquilerDao es la clase de referencia de los metodos de DAO que sería recomendable tener, habría que hacer copia y pega en las demás clases y adaptar los nombres. Jar añadido del JDO.

--- ProyectoCinePlus/src/main/java/es/deusto/spq/ProyectoCinePlus/servidor/DAO/AlquilerDAO.java
+++ ProyectoCinePlus/src/main/java/es/deusto/spq/ProyectoCinePlus/servidor/DAO/AlquilerDAO.java
@@ -1,9 +1,18 @@
 package es.deusto.spq.ProyectoCinePlus.servidor.DAO;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Extent;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
 
 public class AlquilerDAO {
-
+	private PersistenceManagerFactory pmf;
 	private int id_alquiler;
 	private Date fecha_alquiler;
 	private Date fecha_devolucion;
@@ -18,6 +27,159 @@ public class AlquilerDAO {
 		this.inventario = inventario;
 	}
 
+	
+	public AlquilerDAO(){
+		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+	}
+	
+	public void storeAlquiler(AlquilerDAO Alquiler) {
+		this.storeObject(Alquiler);
+	}
+	
+	private void storeObject(AlquilerDAO Alquiler) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+	    Transaction tx = pm.currentTransaction();
+	   
+	    try {
+	       tx.begin();
+	       System.out.println("   * Storing an object: " + Alquiler);
+	       pm.makePersistent(Alquiler);
+	       tx.commit();
+	    } catch (Exception ex) {
+	    	System.out.println("   $ Error storing an object: " + ex.getMessage());
+	    } finally {
+	    	if (tx != null && tx.isActive()) {
+	    		tx.rollback();
+	    	}
+				
+    		pm.close();
+	    }
+	}
+	
+	
+
+	
+	public List<AlquilerDAO> getAlquileres() {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		/* By default only 1 level is retrieved from the db
+		 * so if we wish to fetch more than one level, we must indicate it
+		 */
+		pm.getFetchPlan().setMaxFetchDepth(3);
+		
+		Transaction tx = pm.currentTransaction();
+		List<AlquilerDAO> Alquileres = new ArrayList<AlquilerDAO>();
+		
+		try {
+			System.out.println("   * Retrieving an Extent for Products.");
+			
+			tx.begin();			
+			Extent<AlquilerDAO> extent = pm.getExtent(AlquilerDAO.class, true);
+			
+			for (AlquilerDAO Alquiler : extent) {
+				Alquileres.add(Alquiler);
+			}
+
+			tx.commit();			
+		} catch (Exception ex) {
+	    	System.out.println("   $ Error retrieving an extent: " + ex.getMessage());
+	    } finally {
+	    	if (tx != null && tx.isActive()) {
+	    		tx.rollback();
+	    	}
+
+    		pm.close();    		
+	    }
+	    				
+		return Alquileres;
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	/**
+	 * Para un alquiler concreto
+	 * @param condicion de la WHERE
+	 * @return lista de alquileres que cumpla la condicion
+	 */
+	public List<AlquilerDAO> getAlquileres(String condition) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(3);
+		
+	    Transaction tx = pm.currentTransaction();
+	    List<AlquilerDAO> Alquileres = new ArrayList<AlquilerDAO>();
+	        
+	    try {
+	    	System.out.println("   * Executing a Query for Products given a condition: " + condition);
+	    	
+	    	tx.begin();	    	
+			Extent<AlquilerDAO> extent = pm.getExtent(AlquilerDAO.class, true);
+			Query<AlquilerDAO> query = pm.newQuery(extent, condition);
+
+			for (AlquilerDAO Alquiler : (List<AlquilerDAO>)query.execute()) {
+				Alquileres.add(Alquiler);
+			}
+			
+	        tx.commit();
+	    } catch (Exception ex) {
+	    	System.out.println("   $ Error retreiving an extent: " + ex.getMessage());
+	    } finally {
+	    	if (tx != null && tx.isActive()) {
+	    		tx.rollback();
+	    	}
+			
+    		pm.close();
+	    }
+    
+	    return Alquileres;
+	}
+	
+	public AlquilerDAO getAlquiler(String name){
+		PersistenceManager pm = pmf.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(3);
+		
+		Transaction tx = pm.currentTransaction();
+		AlquilerDAO Alquiler = null;
+	    
+		try {
+			System.out.println ("   * Querying a Product: " + name);
+			
+	    	tx.begin();
+	    	Query<?> query = pm.newQuery("SELECT FROM " + AlquilerDAO.class.getName() + " WHERE email == '" + name + "'");
+	    	query.setUnique(true);
+	    	Alquiler = (AlquilerDAO)query.execute();	    
+ 	    	tx.commit();
+   	    
+	     } catch (Exception ex) {
+		   	System.out.println("   $ Error retreiving an extent: " + ex.getMessage());
+	     } finally {
+		   	if (tx != null && tx.isActive()) {
+		   		tx.rollback();
+		 }
+				
+	   		pm.close();
+	     }
+
+	    return Alquiler;
+	}	
+	
+	
+	public void updateAlquiler(AlquilerDAO Alquiler) {
+		PersistenceManager pm = pmf.getPersistenceManager();
+	    Transaction tx = pm.currentTransaction();
+	    
+	    try {
+	    	tx.begin();
+	    	pm.makePersistent(Alquiler);
+	    	tx.commit();
+	     } catch (Exception ex) {
+		   	System.out.println("   $ Error retreiving an extent: " + ex.getMessage());
+	     } finally {
+		   	if (tx != null && tx.isActive()) {
+		   		tx.rollback();
+		   	}
+				
+	   		pm.close();
+	     }
+	}
 	public int getId_alquiler() {
 		return id_alquiler;
 	}
