38b667b7bd47d9ab240435309f68703ad3ae61bf
mikel <mikel.fdez@opendeusto.es>
2018-04-04 12:32:14 +0200

    m

---
+++ ProyectoCinePlus/src/main/java/es/deusto/spq/ProyectoCinePlus/cliente/util/GUI/Login.java
@@ -0,0 +1,11 @@
+package es.deusto.spq.ProyectoCinePlus.cliente.util.GUI;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO aaAuto-generated method stub
+		System.out.println("aaa");
+
+	}
+
+}
