33eebec34a61642011ddfdfe3629c7abb1ecec12
Aritz <aritzrasines@hotmail.com>
2018-05-03 18:05:12 +0200

    cambios menores 2

--- ProyectoCinePlus/src/test/java/es/deusto/spq/ProyectoCinePlus/servidor/DAO/PeliculaDAOTest.java
+++ ProyectoCinePlus/src/test/java/es/deusto/spq/ProyectoCinePlus/servidor/DAO/PeliculaDAOTest.java
@@ -33,9 +33,9 @@ public class PeliculaDAOTest {
 	public void setUp() throws Exception {
 	  //  peli1 = new Pelicula(1, "Cadena perpetua", 142, "vida de prisioneros", 1994, "Drama", 14, "14");
 	  // peli2 = new Pelicula(2, "Alternativa", 140, "vida de prisioneros 2", 1995, "Suspense", 15,"15");
-	  Pelicula  peli3 = new Pelicula(3, "Alien", 120, "el octavo pasagero", 1979, "Terror", 9,"9");
-	  Pelicula  peli4 = new Pelicula(4, "Star Trek", 128, "el futuro comienza", 2009, "ciencia ficcion", 5,"5");
-	  Pelicula  spq = new Pelicula(5, "Prueba", 0, "prueba", 2018, "SPQ", 10,"10");
+	  Pelicula  peli3 = new Pelicula(3, "Alien", 120, "el octavo pasagero", 1979, "Terror", 9,listUsuarios,"9");
+	  Pelicula  peli4 = new Pelicula(4, "Star Trek", 128, "el futuro comienza", 2009, "ciencia ficcion", 5,listUsuarios,"5");
+	  Pelicula  spq = new Pelicula(5, "Prueba", 0, "prueba", 2018, "SPQ", 10,listUsuarios,"10");
 	    
 	    peliculaDAO.storePelicula(peli3);
 	    peliculaDAO.storePelicula(peli4);
@@ -46,7 +46,7 @@ public class PeliculaDAOTest {
 	@Test
 	public void testStorePelicula() throws Exception{
 		
-		 Pelicula  peli3 = new Pelicula(3, "Alien", 120, "el octavo pasagero", 1979, "Terror", 9,"9");
+		 Pelicula  peli3 = new Pelicula(3, "Alien", 120, "el octavo pasagero", 1979, "Terror", 9,listUsuarios,"9");
 
 		peliculaDAO.storePelicula(peli3);
 		Pelicula prueba2 = peliculaDAO.getPelicula("Alien");
@@ -60,7 +60,7 @@ public class PeliculaDAOTest {
 	
 	@Test
 	public void getUPeliculaTest() throws Exception{
-		 Pelicula  peli1 = new Pelicula(1, "Cadena perpetua", 142, "vida de prisioneros", 1994, "Drama", 14, "14");
+		 Pelicula  peli1 = new Pelicula(1, "Cadena perpetua", 142, "vida de prisioneros", 1994, "Drama", 14,listUsuarios, "14");
 
 		Pelicula prueba2 = peliculaDAO.getPelicula("Alien");
 		assertEquals(peli1.getNombre(), prueba2.getNombre());
@@ -68,7 +68,7 @@ public class PeliculaDAOTest {
 	
 	@Test
 	public void updateUsuarioTest() throws Exception{
-		Pelicula prueba1 = new Pelicula (6, "Star Wars", 125, "Una nueva esperanza", 1970, "ciencia ficcion", 6,"6");
+		Pelicula prueba1 = new Pelicula (6, "Star Wars", 125, "Una nueva esperanza", 1970, "ciencia ficcion", 6,listUsuarios,"6");
 		peliculaDAO.storePelicula(prueba1);
 		
 		Pelicula prueba2 = peliculaDAO.getPelicula("Star Wars");
