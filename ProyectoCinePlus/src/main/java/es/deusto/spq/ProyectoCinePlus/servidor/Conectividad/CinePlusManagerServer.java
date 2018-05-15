1c5c4ba48acc837770f5ad6baedbbeb049164ea0
Aritz <aritzrasines@hotmail.com>
2018-05-15 16:16:23 +0200

    pequeño fix del main del server

--- ProyectoCinePlus/src/main/java/es/deusto/spq/ProyectoCinePlus/servidor/Conectividad/CinePlusManagerServer.java
+++ ProyectoCinePlus/src/main/java/es/deusto/spq/ProyectoCinePlus/servidor/Conectividad/CinePlusManagerServer.java
@@ -17,9 +17,7 @@ public class CinePlusManagerServer  {
 		if (args.length != 3) {
 			System.exit(0);
 		}
-		
-		Thread rmiRegistryThread = null;
-		Thread rmiServerThread = null;
+	
 
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
@@ -27,68 +25,30 @@ public class CinePlusManagerServer  {
 		
 		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
 
-		class RMIRegistryRunnable implements Runnable {
+		logger.info("This is a test to check how mvn test executes this test without external interaction; JVM properties by program");
+		System.setProperty("java.security.policy", "target\\classes\\security\\java.policy");
 
-			public void run() {
-				try {
-					//java.rmi.registry.LocateRegistry.createRegistry(33060);
-					logger.info("RMI registry ready.");
-				} catch (Exception e) {
-					logger.info("Exception starting RMI registry:");
-					e.printStackTrace();
-				}	
-			}
-		}
-		
-		rmiRegistryThread = new Thread(new RMIRegistryRunnable());
-		rmiRegistryThread.start();
+
 		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException ie) {
-			ie.printStackTrace();
+			ICinePlus objServer = new CinePlusServer();
+			Naming.rebind(name, objServer);
+			logger.info("Server '" + name + "' active and waiting...");
+			InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+			BufferedReader stdin = new BufferedReader(inputStreamReader);
+	        String line = stdin.readLine();
+	       
+	       
+		} catch (RemoteException re) {
+			logger.debug(" # Collector RemoteException: " + re.getMessage());
+			re.printStackTrace();
+			System.exit(-1);
+		} catch (MalformedURLException murle) {
+			logger.debug(" # Collector MalformedURLException: " + murle.getMessage());
+			murle.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		class RMIServerRunnable implements Runnable {
 
-		
-			public void run() {
-				logger.info("This is a test to check how mvn test executes this test without external interaction; JVM properties by program");
-				System.setProperty("java.security.policy", "target\\classes\\security\\java.policy");
-
-
-				try {
-					ICinePlus objServer = new CinePlusServer();
-					Naming.rebind(name, objServer);
-					logger.info("Server '" + name + "' active and waiting...");
-					InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-					BufferedReader stdin = new BufferedReader(inputStreamReader);
-			        String line = stdin.readLine();
-			       
-			       
-				} catch (RemoteException re) {
-					logger.debug(" # Collector RemoteException: " + re.getMessage());
-					re.printStackTrace();
-					System.exit(-1);
-				} catch (MalformedURLException murle) {
-					logger.debug(" # Collector MalformedURLException: " + murle.getMessage());
-					murle.printStackTrace();
-					System.exit(-1);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			}
-		}
-		rmiServerThread = new Thread(new RMIServerRunnable());
-		rmiServerThread.start();
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException ie) {
-			ie.printStackTrace();
-		}
-	
-
-		
 	}
-
 }
