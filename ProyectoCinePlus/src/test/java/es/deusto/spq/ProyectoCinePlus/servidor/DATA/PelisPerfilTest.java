7bc5571e95ce819980d32b1855fdf9212568e087
Javier Fernandez <javi_fercol06@hotmail.com>
2018-05-17 15:53:57 +0200

    Nuevos test

---
+++ ProyectoCinePlus/src/test/java/es/deusto/spq/ProyectoCinePlus/servidor/DATA/PelisPerfilTest.java
@@ -0,0 +1,37 @@
+package es.deusto.spq.ProyectoCinePlus.servidor.DATA;
+
+import static org.junit.Assert.*;
+
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PelisPerfilTest {
+	
+	static Logger logger = Logger.getLogger(PelisPerfilTest.class.getName());
+	
+	private static PelisPerfil pelisperfil1;
+	private static PelisPerfil pelisperfil2;
+	private static String aritz;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		pelisperfil1 = new PelisPerfil("javi@gmail.com",1);
+		pelisperfil2 = new PelisPerfil("mikel@gmail.com",3);
+		aritz="aritz@gmail.com";
+	}
+
+	@Test
+	public void comprobarDatosTest() {
+		assertEquals("mikel@gmail.com",pelisperfil2.getEmail());
+		assertEquals(3,pelisperfil2.getId_pelicula());
+	}
+	
+	@Test
+	public void setEmailTest() {
+		pelisperfil1.setEmail("aritz@gmail.com");
+		assertEquals(aritz, pelisperfil1.getEmail());
+	}
+
+}
