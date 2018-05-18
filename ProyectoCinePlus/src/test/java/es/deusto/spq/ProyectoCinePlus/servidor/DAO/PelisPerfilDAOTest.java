0fe99ca8162aaf93b79425262d1892f295cd1003
mikel <mikel.fdez@opendeusto.es>
2018-05-18 09:23:16 +0200

    Nuevos test añadidos

--- ProyectoCinePlus/src/test/java/es/deusto/spq/ProyectoCinePlus/servidor/DAO/PelisPerfilDAOTest.java
+++ ProyectoCinePlus/src/test/java/es/deusto/spq/ProyectoCinePlus/servidor/DAO/PelisPerfilDAOTest.java
@@ -21,10 +21,10 @@ import junit.framework.JUnit4TestAdapter;
 public class PelisPerfilDAOTest {
 	
 	private static PelisPerfilDAO pelisPerfilDAO;
-	private static PelisPerfil pelisperfil1;
-	private static PelisPerfil pelisperfil2;
-	private static PelisPerfil pelisperfil3;
-	private static String javi;
+	
+	private  PelisPerfil pelisperfil1;
+	private  PelisPerfil pelisperfil2;
+	private  PelisPerfil pelisperfil3;
 	
 	static Logger logger = Logger.getLogger(PelisPerfilDAOTest.class.getName());
 	
@@ -37,36 +37,71 @@ public class PelisPerfilDAOTest {
 	
 	
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		javi= "javitxu@gmail.com";
-		pelisperfil1 = new PelisPerfil(javi,1);
-		pelisperfil2 = new PelisPerfil("mikel@gmail.com",3);
-		pelisperfil3 = new PelisPerfil(javi,5);
-		
+	public static void setUpBeforeClass() throws Exception{
 		pelisPerfilDAO = new PelisPerfilDAO();
-		pelisPerfilDAO.storePelisPerfil(pelisperfil3);
+
 	}
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception{
+		pelisperfil1 = new PelisPerfil("javitxu@gmail.com",1);
+		pelisperfil2 = new PelisPerfil("mikel@gmail.com",3);
+		pelisperfil3 = new PelisPerfil("javitxu@gmail.com",5);
+		
+		 if(pelisPerfilDAO.checkPelis(pelisperfil1) == false){
+			 pelisPerfilDAO.storePelisPerfil(pelisperfil1);
+			 logger.info("almacenando "+pelisperfil1.getId_pelicula());
+			
+		 }
+		 if(pelisPerfilDAO.checkPelis(pelisperfil2) == false){
+			 pelisPerfilDAO.storePelisPerfil(pelisperfil2);
+			 logger.info("almacenando "+pelisperfil2.getId_pelicula());
+			
+		 }
 	}
 
 
 	@Test
-	public void StorePelisPerfiltest() {
-		assertTrue(pelisPerfilDAO.storePelisPerfil(pelisperfil1));
+	public void StorePelisPerfiltest(){
+		assertTrue(pelisPerfilDAO.storePelisPerfil(pelisperfil3));
 	}
 
 	@Test
-	public void StoreObjetcttest() {
+	public void StoreObjetcttest(){
 		pelisPerfilDAO.storePelisPerfil(pelisperfil2);
 		assertTrue(pelisPerfilDAO.checkPelis(pelisperfil2));
+	}
+	
+	@Test
+	public void deleteObjectTest(){
+		pelisPerfilDAO.deletePelisPerfil(pelisperfil1);
+		assertFalse(pelisPerfilDAO.checkPelis(pelisperfil1));
 	}
 
 	@Test
 	@Required(totalTime = 300)
-	public void getPeliUsuarioTest() {
-		assertTrue(pelisPerfilDAO.getPeliUsuario(javi).size()>0);
+	public void getPeliUsuarioTest(){
+		assertTrue(pelisPerfilDAO.getPeliUsuario("javitxu@gmail.com").size()>0);
+	}
+	
+	@After
+	public void deleteUsers() throws Exception{
+		 if(pelisPerfilDAO.checkPelis(pelisperfil1) == true){
+			 pelisPerfilDAO.deletePelisPerfil(pelisperfil1);
+			 logger.info("almacenando "+pelisperfil1.getId_pelicula());
+			
+		 }
+		 if(pelisPerfilDAO.checkPelis(pelisperfil2) == true){
+			 pelisPerfilDAO.deletePelisPerfil(pelisperfil2);
+			 logger.info("almacenando "+pelisperfil2.getId_pelicula());
+			
+		 }
+		 if(pelisPerfilDAO.checkPelis(pelisperfil3) == true){
+			 pelisPerfilDAO.deletePelisPerfil(pelisperfil3);
+			 logger.info("almacenando "+pelisperfil3.getId_pelicula());
+			
+		 }
+		  
 	}
 	
 	
